dfd712c5b7ba5062e7cacf0b0aa4e8cd167e25d4
Nikita Adamov <nikita_adamov@epam.com>
2022-04-01 02:10:30 +0530

    refactor Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,11 +5,21 @@ import styled from 'styled-components';
 const Header = () => {
   return (
     <Wrapper>
-      <Container>Header</Container>
+      <Container>
+        <Logo>Onion News</Logo>
+      </Container>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.header``;
+const Wrapper = styled.div`
+  padding: 20px;
+  border-bottom: 3px solid black;
+`;
+
+const Logo = styled.div`
+  font-family: 'VT323', monospace;
+  font-size: 56px;
+`;
 
 export default Header;
