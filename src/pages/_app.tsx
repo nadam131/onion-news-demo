c2a7c17a95385ea49e9facb936106f2ebc5d5ff6
Nikita Adamov <nikita_adamov@epam.com>
2022-04-03 00:00:46 +0530

    update globalstyles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,35 +1,10 @@
 import type { AppProps } from 'next/app';
-import { Normalize } from 'styled-normalize';
-
 import Layout from '@components/Layout/Layout';
-
-import { createGlobalStyle } from 'styled-components';
-
-const GlobalStyles = createGlobalStyle`
-
-html,body {
-  font-family: 'VT323', monospace; 
-}
-
-a {
-    color: inherit;
-    text-decoration: none;
-}
-
-a:hover {
-  text-decoration: underline;
-}
-
-* {
-    box-sizing: border-box;
-}
-`;
+import '@styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Normalize />
-      <GlobalStyles />
       <Layout>
         <Component {...pageProps} />
       </Layout>
